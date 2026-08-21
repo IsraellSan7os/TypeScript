22b544bed69671962e970e0da32d83f61ed1aede
Israel Santos <ysraellsanosss@gmail.com>
2026-08-21 12:13:00 -0300

    type any

--- dist/estudos.js
+++ dist/estudos.js
@@ -1,3 +1,30 @@
 "use strict";
-const messagee = "Hellow type-script";
-const nome = "Israel custodio dos santos";
+// Tipos primitivos
+/*
+string
+number
+boolean
+any
+*/
+// String pra texto.
+let username;
+username = "Israel";
+// Number para números
+let total;
+total = 4;
+// Recebe números quebrados. No Brasil usamos vírgula, mas na programação usamos ponto.
+total = 4.1;
+// Boolean para verdadeiro ou falso
+// Tipagem explicita
+let isLoading;
+isLoading = true;
+isLoading = false;
+// Tipagem explicita define claramente o tipo da variavel
+// enquanto a inferência de tipos permite que o TypeScript deduza o tipo com base no valor atribuído.
+// inferencia de tipos
+// tipagem implicita
+let showMessage = "Olá, tudo bem?";
+let num = 12;
+console.log(showMessage);
+
+
--- estudos.ts
+++ estudos.ts
@@ -3,6 +3,7 @@
 string
 number
 boolean
+any => aceita qualquer coisa
 */
 
 // String pra texto.
@@ -16,6 +17,29 @@ total = 4;
 total = 4.1;
 
 // Boolean para verdadeiro ou falso
-let isLoading: boolean 
-isLoading = true
-isLoading = false
+// Tipagem explicita
+let isLoading: boolean;
+isLoading = true;
+isLoading = false;
+
+// Tipagem explicita define claramente o tipo da variavel
+// enquanto a inferência de tipos, TypeScript deduza o tipo com base no valor atribuído.
+
+// inferencia de tipos
+// tipagem implicita
+let showMessage = "Ola tudo bem??";
+let num = 12;
+
+console.log(showMessage);
+
+const message = document.querySelector<HTMLParagraphElement>("#message");
+
+if (message) {
+  message.textContent = showMessage;
+}
+
+// Quando nao definimos o type ele define altomaticamente
+let n: any;
+n = "Israel";
+n = 12;
+
